758766be49c3039ddaaeefbefa89e34b27962636
Ana <jacque.clara@hotmail.com>
2020-02-20 14:52:50 -0300

    Exercicio do aniversário com mensagem

--- exercicio1.py
+++ exercicio1.py
@@ -4,8 +4,11 @@ def dias_de_vida(dia,mes,ano):
     data1=datetime.date(year=ano,month=mes,day=dia)
     z=datetime.date.today()
     return (z-data1)
+
 dia=int(input("Insira o dia do seu aniversário: "))
 mes=int(input("Insira o mes do seu aniversário: "))
 ano=int(input("Insira o ano do seu aniversário: "))
-print(dias_de_vida(dia,mes,ano))
+x=dias_de_vida(dia,mes,ano)
+
+print("Você tem ", x," dias de vida")
 
